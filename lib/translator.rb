9ad2e354c8903be61df30db8aaeba531beb3937b
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-10 00:44:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
 # require modules here
-
-def load_library
+require 'yaml'
+def load_library(path)
   # code goes here
+  emotes = YAML.load_file('./lib/emoticons.yml')
+  emotes.reduce(nil) do |memo,(key,value)|
+    memo = {}
+    memo[key]={:Japanese => value[1],:American => value[0]}
+    memo
+  end
+  emotes
 end
 
 def get_japanese_emoticon
